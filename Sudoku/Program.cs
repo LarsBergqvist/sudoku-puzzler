0d751d1c84ec3e5fc874619742a137bf995edfe0
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2025-02-02 20:57:36 +0100

    Added menu to select difficulty level

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace Sudoku
+﻿using System;
+namespace Sudoku
 {
     internal static class Program
     {
@@ -8,10 +9,37 @@
             var validator = new GridValidator();
             var solver = new SudokuSolver(validator);
             var generator = new SudokuGenerator(validator, solver, printer);
-            var policy = new HardPuzzlePolicy();
+
+            printer.WriteLine("Select difficulty level:");
+            printer.WriteLine("1. Basic");
+            printer.WriteLine("2. Hard");
+            printer.WriteLine("3. Very Hard");
+
+            IPuzzlePolicy policy;
+            while (true)
+            {
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        policy = new BasicPuzzlePolicy();
+                        break;
+                    case "2":
+                        policy = new HardPuzzlePolicy();
+                        break;
+                    case "3":
+                        policy = new VeryHardPuzzlePolicy();
+                        break;
+                    default:
+                        printer.WriteLine("Invalid selection. Please enter 1, 2, or 3.");
+                        continue;
+                }
+                break;
+            }
+
             var sudokuPuzzle = generator.GeneratePuzzle(policy);
             new SudokuPrinter(sudokuPuzzle, printer).Print();
             printer.WriteLine($"Num blanks in puzzle: {validator.GetNumBlanks(sudokuPuzzle.PuzzleGrid)}");
-      }
+        }
     }
 }
